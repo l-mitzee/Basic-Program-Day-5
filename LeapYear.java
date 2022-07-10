dfd3d1a48684eb29010c1d16823d4fc468d9d562
Lisa <lisa.das9374@gmail.com>
2022-07-11 00:13:30 +0530

    Added:Year is a leap year or not

---
+++ LeapYear.java
@@ -0,0 +1,22 @@
+package Java_Basic;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		int year;
+		Scanner input = new Scanner(System.in);
+		year = input.nextInt();
+		if(((year % 4 == 0) && (year % 100!= 0)) || (year % 400 == 0)) {
+			System.out.println("Given year is a leap year");
+		}
+		else
+		{
+			System.out.println("Given year is not a leap year");
+		}
+	
+	
+	}
+
+
+}
